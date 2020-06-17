df12e5a4c6114244286510d07f1f97fb574c485b
morad <morad@shimaatech.com>
2020-06-18 01:42:45 +0300

    added retrofit generated file to git

---
+++ repository/lib/src/remote/dio_rest_client.g.dart
@@ -0,0 +1,69 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dio_rest_client.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+class __DioRestClient implements _DioRestClient {
+  __DioRestClient(this._dio, {this.baseUrl}) {
+    ArgumentError.checkNotNull(_dio, '_dio');
+  }
+
+  final Dio _dio;
+
+  String baseUrl;
+
+  @override
+  _get(id) async {
+    ArgumentError.checkNotNull(id, 'id');
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final Response<Map<String, dynamic>> _result = await _dio.request('/$id',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'GET',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = MapWrapper.fromJson(_result.data);
+    return value;
+  }
+
+  @override
+  delete(id) async {
+    ArgumentError.checkNotNull(id, 'id');
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    await _dio.request<void>('/$id',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'DELETE',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    return null;
+  }
+
+  @override
+  _post(entity) async {
+    ArgumentError.checkNotNull(entity, 'entity');
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{r'data': entity?.toJson()};
+    final _data = <String, dynamic>{};
+    await _dio.request<void>('/',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'POST',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    return null;
+  }
+}
