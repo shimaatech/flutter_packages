52b5c20692a85c5d3fce8331ac112a8c3e337750
morad <morad@shimaatech.com>
2020-06-26 15:41:56 +0300

    app home

---
+++ app_home/lib/src/app_home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppHome extends StatelessWidget {
+
+  final AppStore
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
