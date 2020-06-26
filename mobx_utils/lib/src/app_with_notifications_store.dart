9b6cddc4de9699d40d2b2a03ede86a930a769452
morad <morad@shimaatech.com>
2020-06-26 14:16:55 +0300

    app store with notifications

--- mobx_utils/lib/src/app_with_notifications_store.dart
+++ mobx_utils/lib/src/app_with_notifications_store.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/foundation.dart';
 import 'package:mobx/mobx.dart';
 import 'package:mobx_utils/src/notifications_store.dart';
-import 'package:notifications/src/notifications.dart';
+import 'package:notifications/notifications.dart';
 
 import '../mobx_utils.dart';
 
 part 'app_with_notifications_store.g.dart';
 
-
-class AppWithNotificationsStore = _AppWithNotificationsStore with _$AppWithNotificationsStore;
+class AppWithNotificationsStore = _AppWithNotificationsStore
+    with _$AppWithNotificationsStore;
 
 abstract class _AppWithNotificationsStore extends BaseStore with Store {
-
   @protected
   final NotificationsStore notificationsStore;
 
   _AppWithNotificationsStore(this.notificationsStore);
 
-  NotificationMessage get clickedNotification
+  @computed
+  NotificationMessage get clickedNotification =>
+      notificationsStore.clickedNotification;
 
+  @computed
+  NotificationMessage get receivedNotification =>
+      notificationsStore.receivedNotification;
 }
