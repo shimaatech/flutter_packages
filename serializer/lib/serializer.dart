b5ba6b63e83340248ff65a9bec90ba135995572b
morad <morad@shimaatech.com>
2020-07-12 00:55:21 +0300

    added UtcIsoDateConverter

--- serializer/lib/serializer.dart
+++ serializer/lib/serializer.dart
@@ -1,5 +1,7 @@
 library serializer;
 
+import 'package:json_annotation/json_annotation.dart';
+
 typedef Deserialize<T> = T Function(Map<String, dynamic> json);
 typedef Serialize<T> = Map<String, dynamic> Function(T obj);
 
@@ -10,5 +12,33 @@ class Serializer<T> {
 
   Serializer(this.serialize, this.deserialize);
 
+}
+
+
+class UtcIsoDateConverter implements JsonConverter<DateTime, String> {
+  const UtcIsoDateConverter();
+
+  @override
+  DateTime fromJson(String json) {
+    if (json == null) {
+      return null;
+    }
+    return DateTime.parse(json);
+  }
+
+  @override
+  String toJson(DateTime datetime) {
+    if (datetime == null) {
+      return null;
+    }
+    if (!datetime.isUtc) {
+      throw Exception('DateTime must be in utc');
+    }
+    String json = datetime.toIso8601String();
+    if (json.contains('.')) {
+      json = '${json.replaceRange(json.indexOf('.'), json.length, '')}Z';
+    }
+    return json;
+  }
 }
 
