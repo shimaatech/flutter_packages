eee65ccf997651953497edf1b1a8605381054fae
morad <morad@shimaatech.com>
2020-09-25 12:33:42 +0300

    localizations

--- app_update/lib/app_update.dart
+++ app_update/lib/app_update.dart
@@ -2,3 +2,4 @@ library app_update;
 
 export 'src/app_info_service.dart';
 export 'src/app_update_service.dart';
+export 'src/localizations/localizations.dart';
---
+++ app_update/lib/src/localizations/localizations.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:multiple_localization/multiple_localization.dart';
+
+import 'l10n/messages_all.dart';
+
+class UpdateDialogLocalizations {
+  static Future<UpdateDialogLocalizations> load(Locale locale) {
+    return MultipleLocalizations.load(
+        initializeMessages, locale, (locale) => UpdateDialogLocalizations(),
+        setDefaultLocale: true);
+  }
+
+  static UpdateDialogLocalizations of(BuildContext context) {
+    return Localizations.of<UpdateDialogLocalizations>(
+        context, UpdateDialogLocalizations);
+  }
+
+  static const List<Locale> supportedLocales = const [
+    Locale('en'),
+    Locale('he'),
+    Locale('ar'),
+  ];
+
+  // Static member to have a simple access to the delegate from the MaterialApp
+  static LocalizationsDelegate<UpdateDialogLocalizations> delegate() =>
+      _UpdateDialogLocalizationsDelegate();
+
+  String get title => Intl.message('App Update', name: 'title');
+
+  String get flexibleUpdateMessage => Intl.message(
+      'A new update is available. Please update in order to get the latest features',
+      name: 'flexibleUpdateMessage');
+
+  String get immediateUpdateMessage => Intl.message(
+      'We enhanced the app for you. Please update the app in order to continue',
+      name: 'immediateUpdateMessage');
+
+  String get update => Intl.message('Update', name: 'update');
+
+  String get later => Intl.message('Later', name: 'later');
+}
+
+class _UpdateDialogLocalizationsDelegate
+    extends LocalizationsDelegate<UpdateDialogLocalizations> {
+  @override
+  bool isSupported(Locale locale) {
+    return UpdateDialogLocalizations.supportedLocales.contains(locale);
+  }
+
+  @override
+  Future<UpdateDialogLocalizations> load(Locale locale) {
+    return UpdateDialogLocalizations.load(locale);
+  }
+
+  @override
+  bool shouldReload(LocalizationsDelegate<UpdateDialogLocalizations> old) {
+    return false;
+  }
+}
