c7cf58c02d2882a58b44cca258fce30f5a2a7df3
morad <morad@shimaatech.com>
2020-12-09 19:36:52 +0200

    reverting back to old webview

--- website_viewer/lib/src/website_viewer.dart
+++ website_viewer/lib/src/website_viewer.dart
@@ -1,60 +1,58 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:general_utils/general_utils.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
-typedef WebsiteViewerErrorCallback = Function(
-    String mainUrl, String failingUrl, int code, String message);
+typedef WebsiteViewerErrorCallback = Function(int code, String message);
 
 class WebsiteViewer extends StatefulWidget {
-  WebsiteViewer(this.url, {this.onError});
+  WebsiteViewer(this.url, {this.onLoadError, this.onLoadHttpError});
 
   final String url;
 
-  final WebsiteViewerErrorCallback onError;
+  final WebsiteViewerErrorCallback onLoadError;
+  final WebsiteViewerErrorCallback onLoadHttpError;
 
   @override
   _WebsiteViewerState createState() => _WebsiteViewerState();
 }
 
 class _WebsiteViewerState extends State<WebsiteViewer> {
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
-  }
+  double _progress = 0;
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        if (_isLoading) LinearProgressIndicator(),
+      children: <Widget>[
+        (_progress != 1.0)
+            ? LinearProgressIndicator(value: _progress)
+            : Container(),
         Expanded(
-          child: WebView(
-            initialUrl: widget.url,
-            javascriptMode: JavascriptMode.unrestricted,
-            onPageFinished: (finish) {
-              setState(() {
-                _isLoading = false;
-              });
-            },
-            onWebResourceError: (error) {
-              if (widget.onError != null) {
-                widget.onError(
-                  widget.url,
-                  error.failingUrl,
-                  error.errorCode,
-                  error.description,
-                );
+          child: InAppWebView(
+            onLoadError: (controller, url, code, message) {
+              if (widget.onLoadError != null) {
+                widget.onLoadError(code, message);
               }
             },
+            onLoadHttpError: (controller, url, code, message) {
+              if (widget.onLoadHttpError != null) {
+                widget.onLoadHttpError(code, message);
+              }
+            },
+            initialUrl: widget.url,
+            initialOptions: InAppWebViewGroupOptions(
+              crossPlatform: InAppWebViewOptions(debuggingEnabled: false),
+            ),
+            onProgressChanged: (controller, progress) => _setProgress(progress),
           ),
         ),
       ],
     );
+  }
+
+  void _setProgress(int progress) {
+    setState(() {
+      _progress = progress / 100;
+    });
   }
 }
 
